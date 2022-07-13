c80750427f08f84b5e74549371719dcf03667af1
Hayden Brown <nedyah.brown@gmail.com>
2022-07-13 11:20:20 +0000

    alacritty and fish

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+#   _  _   ___
+#  | || | | _ )    Hayden Brown (htbrown.com)
+#  | __ | | _ \
+#  |_||_| |___/    dotfiles > FISH
+#
+
+#--------#
+#  Misc  #
+#--------#
+
+# Starship
+starship init fish | source
